ccb4b340ab9a488b454eccdf7a115317b6a3b771
JosephG1505 <jjguerre@espol.edu.ec>
2016-08-04 23:46:13 -0500

    Primera Actualizacion
    Subiendo la primera actualizacion de TyperShark_Game

---
+++ src/typershark_game/TyperShark_Game.java
@@ -0,0 +1,20 @@
+
+package typershark_game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class TyperShark_Game extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
